13747be21c1a1b02d44f63b727b0d446765074d9
KOYAMA Hiroshi <koyama@kyoto-msc.jp>
2026-01-18 11:43:13 +0900

    fix: スクロールできない不具合を修正

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -219,7 +219,7 @@ export default function StartPage() {
                           "backdrop-blur-md",
                           active
                             ? "border-white/70 bg-sky-300/90"
-                            : "border-white/45 bg-sky-100/45 hover:bg-sky-200/45",
+                            : "border-white/45 bg-sky-200/90 hover:bg-sky-200/45",
                           disabled ? "opacity-60" : "opacity-100",
                         ].join(" ")}
                       >
