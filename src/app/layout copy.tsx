ed239f7d494c9fc6d9502291446300909b23fa13
KOYAMA Hiroshi <koyama@kyoto-msc.jp>
2026-04-27 17:03:56 +0900

    Update

--- src/app/layout copy.tsx
+++ src/app/layout copy.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body className="h-screen overflow-y-auto">
+      <body className="h-screen overflow-hidden">
         <div className="h-screen overflow-hidden">{children}</div>
       </body>
     </html>
